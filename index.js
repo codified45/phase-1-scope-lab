fe4203204ba29ffe7de5b72a0acb53a110e491da
Jack Lach <jack45k@gmail.com>
2023-01-22 00:36:51 -0500

    phase 1 scope lab - all 5 tests passing

--- index.js
+++ index.js
@@ -2,4 +2,19 @@
 
 var customerName = 'bob';
 
+//arrow notation is boss
 upperCaseCustomerName = () => customerName = customerName.toUpperCase();
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'John';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Paul';
+}
